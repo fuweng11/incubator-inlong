1b7aaad9dd4d1900f73a1fefd1efa81b6a7dd6a3
dockerzhang <dockerzhang@tencent.com>
2022-10-27 17:22:51 +0800

    Agent监控支持上报到智研

---
+++ inlong-agent/agent-common/src/main/java/org/apache/inlong/agent/metrics/AgentZhiyanMetricListener.java
@@ -0,0 +1,138 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.inlong.agent.metrics;
+
+import com.google.gson.Gson;
+import com.tencent.teg.monitor.sdk.CurveReporter;
+import com.tencent.teg.monitor.sdk.TegMonitor;
+import org.apache.inlong.common.metric.MetricItemValue;
+import org.apache.inlong.common.metric.MetricListener;
+import org.apache.inlong.common.metric.MetricValue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.apache.inlong.agent.metrics.AgentMetricItem.KEY_COMPONENT_NAME;
+import static org.apache.inlong.agent.metrics.AgentMetricItem.KEY_INLONG_GROUP_ID;
+import static org.apache.inlong.agent.metrics.AgentMetricItem.KEY_INLONG_STREAM_ID;
+import static org.apache.inlong.agent.metrics.AgentMetricItem.KEY_PLUGIN_ID;
+import static org.apache.inlong.agent.metrics.AgentMetricItem.M_JOB_FATAL_COUNT;
+import static org.apache.inlong.agent.metrics.AgentMetricItem.M_JOB_RUNNING_COUNT;
+import static org.apache.inlong.agent.metrics.AgentMetricItem.M_PLUGIN_READ_COUNT;
+import static org.apache.inlong.agent.metrics.AgentMetricItem.M_PLUGIN_READ_FAIL_COUNT;
+import static org.apache.inlong.agent.metrics.AgentMetricItem.M_PLUGIN_READ_SUCCESS_COUNT;
+import static org.apache.inlong.agent.metrics.AgentMetricItem.M_PLUGIN_SEND_COUNT;
+import static org.apache.inlong.agent.metrics.AgentMetricItem.M_PLUGIN_SEND_FAIL_COUNT;
+import static org.apache.inlong.agent.metrics.AgentMetricItem.M_PLUGIN_SEND_SUCCESS_COUNT;
+import static org.apache.inlong.agent.metrics.AgentMetricItem.M_SINK_FAIL_COUNT;
+import static org.apache.inlong.agent.metrics.AgentMetricItem.M_SINK_SUCCESS_COUNT;
+import static org.apache.inlong.agent.metrics.AgentMetricItem.M_SOURCE_FAIL_COUNT;
+import static org.apache.inlong.agent.metrics.AgentMetricItem.M_SOURCE_SUCCESS_COUNT;
+import static org.apache.inlong.agent.metrics.AgentMetricItem.M_TASK_FATAL_COUNT;
+import static org.apache.inlong.agent.metrics.AgentMetricItem.M_TASK_RETRYING_COUNT;
+import static org.apache.inlong.agent.metrics.AgentMetricItem.M_TASK_RUNNING_COUNT;
+
+public class AgentZhiyanMetricListener implements MetricListener {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(AgentZhiyanMetricListener.class);
+    private static MetricValue ZERO = MetricValue.of(null, 0);
+
+    private String appMark;
+    private String metricGroup;
+    private String env;
+    private String instanceMark;
+
+    /**
+     * Constructor
+     */
+    public AgentZhiyanMetricListener() {
+        try {
+            TegMonitor.init();
+            Map<String, String> envMap = System.getenv();
+            this.appMark = envMap.getOrDefault("zhiyan.appMark", "app1");
+            this.metricGroup = envMap.getOrDefault("zhiyan.metricGroup", "groupB");
+            this.env = envMap.getOrDefault("zhiyan.env", "prod");
+            this.instanceMark = envMap.getOrDefault("zhiyan.instanceMark", "127.0.0.1");
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public void snapshot(String domain, List<MetricItemValue> itemValues) {
+        try {
+            Gson gson = new Gson();
+            for (MetricItemValue itemValue : itemValues) {
+                CurveReporter reporter = TegMonitor.curveReporter()
+                        .appMark(this.appMark)
+                        .metricGroup(this.metricGroup)
+                        .env(this.env)
+                        .instanceMark(this.instanceMark);
+                Map<String, String> dimensionMap = itemValue.getDimensions();
+                // dimension
+                reporter.tag(KEY_PLUGIN_ID, dimensionMap.getOrDefault(KEY_PLUGIN_ID, "-"));
+                reporter.tag(KEY_INLONG_GROUP_ID, dimensionMap.getOrDefault(KEY_INLONG_GROUP_ID, "-"));
+                reporter.tag(KEY_INLONG_STREAM_ID, dimensionMap.getOrDefault(KEY_INLONG_STREAM_ID, "-"));
+                reporter.tag(KEY_COMPONENT_NAME, dimensionMap.getOrDefault(KEY_COMPONENT_NAME, "-"));
+                reporter.sumMetric("request_count", Math.random());
+
+                // metric
+                Map<String, MetricValue> metricMap = itemValue.getMetrics();
+                reporter.sumMetric(M_JOB_RUNNING_COUNT,
+                        (double) metricMap.getOrDefault(M_JOB_RUNNING_COUNT, ZERO).value);
+                reporter.sumMetric(M_JOB_FATAL_COUNT,
+                        (double) metricMap.getOrDefault(M_JOB_FATAL_COUNT, ZERO).value);
+                reporter.sumMetric(M_TASK_RUNNING_COUNT,
+                        (double) metricMap.getOrDefault(M_TASK_RUNNING_COUNT, ZERO).value);
+                reporter.sumMetric(M_TASK_RETRYING_COUNT,
+                        (double) metricMap.getOrDefault(M_TASK_RETRYING_COUNT, ZERO).value);
+                reporter.sumMetric(M_TASK_FATAL_COUNT,
+                        (double) metricMap.getOrDefault(M_TASK_FATAL_COUNT, ZERO).value);
+                reporter.sumMetric(M_SINK_SUCCESS_COUNT,
+                        (double) metricMap.getOrDefault(M_SINK_SUCCESS_COUNT, ZERO).value);
+                reporter.sumMetric(M_SINK_FAIL_COUNT,
+                        (double) metricMap.getOrDefault(M_SINK_FAIL_COUNT, ZERO).value);
+                reporter.sumMetric(M_SOURCE_SUCCESS_COUNT,
+                        (double) metricMap.getOrDefault(M_SOURCE_SUCCESS_COUNT, ZERO).value);
+                reporter.sumMetric(M_SOURCE_FAIL_COUNT,
+                        (double) metricMap.getOrDefault(M_SOURCE_FAIL_COUNT, ZERO).value);
+                reporter.sumMetric(M_PLUGIN_READ_COUNT,
+                        (double) metricMap.getOrDefault(M_PLUGIN_READ_COUNT, ZERO).value);
+                reporter.sumMetric(M_PLUGIN_SEND_COUNT,
+                        (double) metricMap.getOrDefault(M_PLUGIN_SEND_COUNT, ZERO).value);
+                reporter.sumMetric(M_PLUGIN_READ_FAIL_COUNT,
+                        (double) metricMap.getOrDefault(M_PLUGIN_READ_FAIL_COUNT, ZERO).value);
+                reporter.sumMetric(M_PLUGIN_SEND_FAIL_COUNT,
+                        (double) metricMap.getOrDefault(M_PLUGIN_SEND_FAIL_COUNT, ZERO).value);
+                reporter.sumMetric(M_PLUGIN_READ_SUCCESS_COUNT,
+                        (double) metricMap.getOrDefault(M_PLUGIN_READ_SUCCESS_COUNT, ZERO).value);
+                reporter.sumMetric(M_PLUGIN_SEND_SUCCESS_COUNT,
+                        (double) metricMap.getOrDefault(M_PLUGIN_SEND_SUCCESS_COUNT, ZERO).value);
+                int result = reporter.report();
+                LOGGER.info("agent-zhiyan,dimensions:{},values:{},result:{}", dimensionMap, gson.toJson(metricMap),
+                        result);
+            }
+        } catch (Exception e) {
+            LOGGER.error("agent report Zhiyan error:{}", e.getMessage(), e);
+        }
+
+    }
+
+}
