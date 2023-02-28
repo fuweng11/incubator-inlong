0f4ad0b52d4707c7f7a9f9e929b2217b2a3a245f
jinghao <jinghao@tencent.com>
2023-02-28 10:48:39 +0800

    [Dashboard][tencent]add FieldMap component (merge request !301)
    Squash merge branch 'fe' into 'internal-dev'
    [Dashboard][tencent]add FieldMap component

---
+++ inlong-dashboard/src/@tencent/components/FieldsMap/index.tsx
@@ -0,0 +1,285 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+import React, { useCallback, useState } from 'react';
+import {
+  Icon,
+  Table,
+  TableProps,
+  Button,
+  Row,
+  Col,
+  Input,
+  Select,
+  StatusTip,
+} from '@tencent/tea-component';
+import styles from './index.module.less';
+import FieldsParse from '../FieldsParse';
+import LongText from '../LongText';
+
+const { injectable, selectable, draggable } = Table.addons;
+
+export type FieldData = {
+  id: number;
+  fieldName: string;
+  fieldType: string;
+  fieldComment?: string;
+}[];
+
+type selectedFieldsProps = {
+  sourceField: FieldData[0];
+  targetField: FieldData[0];
+}[];
+
+export interface ConnectTableProps extends Omit<TableProps<any>, 'columns'> {
+  sourceFields: FieldData;
+  targetFields: FieldData;
+  onSelect: (data: selectedFieldsProps) => void; //get selected fields
+  getTargetFields: (table?: string) => any; //get target fields by table name
+  candoAutoAdd?: boolean; // default true
+}
+
+type actionsProps = {
+  text: string;
+  disabled?: boolean;
+  icon?: React.ReactNode;
+  onClick?: () => void;
+}[];
+
+const FieldsMap = ({
+  sourceFields,
+  targetFields: defaultTargetFields,
+  onSelect,
+  getTargetFields,
+  candoAutoAdd = true,
+  ...props
+}: ConnectTableProps) => {
+  const [fieldsParseVisible, setFieldParseVisible] = useState<boolean>(false);
+  const [selectedFields, setSelectedFields] = useState<selectedFieldsProps>([]);
+  const [targetFields, setTargetFields] = useState<FieldData>(defaultTargetFields);
+  const [status, setStatus] = useState<string>('');
+
+  const actions: actionsProps = [
+    {
+      text: '批量解析字段',
+      onClick: () => {
+        setFieldParseVisible(true);
+      },
+      icon: <Icon type="convertip--blue" />,
+    },
+    {
+      text: '自动添加',
+      icon: <Icon type="and" />,
+      onClick: async () => {
+        setStatus('loading');
+        const newTargetFields = await getTargetFields();
+        setStatus('found');
+        setTargetFields(newTargetFields);
+      },
+      disabled: !candoAutoAdd,
+    },
+    {
+      text: '全部删除',
+      icon: <Icon type="delete" />,
+      onClick: () => {
+        setTargetFields([]);
+      },
+    },
+  ];
+
+  const columns = (type: string): TableProps['columns'] => {
+    return [
+      {
+        key: 'fieldName',
+        header: '源表字段',
+        width: 50,
+        render: records =>
+          type === 'edit' ? (
+            <Input
+              defaultValue={records.fieldName}
+              onChange={val => {
+                onChangeFieldValue('fieldName', records, val);
+              }}
+            />
+          ) : (
+            <LongText text={records.fieldName} style={{ minHeight: '30px', lineHeight: '30px' }} />
+          ),
+      },
+      {
+        key: 'fieldType',
+        header: '类型',
+        width: 50,
+        render: records =>
+          type === 'edit' ? (
+            <Select
+              size="full"
+              appearance="button"
+              defaultValue={records.fieldType}
+              options={['int', 'long', 'float', 'double', 'string', 'date', 'timestamp'].map(
+                item => ({
+                  text: item,
+                  value: item,
+                }),
+              )}
+              onChange={val => {
+                onChangeFieldValue('fieldType', records, val);
+              }}
+            />
+          ) : (
+            <LongText text={records.fieldType} style={{ minHeight: '30px', lineHeight: '30px' }} />
+          ),
+      },
+      {
+        key: 'fieldComment',
+        header: '备注',
+        width: 50,
+        render: records => <LongText text={records.fieldComment} />,
+      },
+    ];
+  };
+
+  //listen target fields change
+  const onChangeFieldValue = (type: 'fieldType' | 'fieldName', records, val) => {
+    const copyTargetFields = targetFields;
+    for (let targetField of copyTargetFields) {
+      if (targetField.id === records.id) {
+        targetField[type] = val;
+      }
+    }
+    setTargetFields(copyTargetFields);
+  };
+
+  return (
+    <>
+      <Row style={{ background: '#f2f2f2', margin: 0 }}>
+        <Col span={12}>
+          <div className={styles.leftCol}>
+            <div className={styles.sourceTable}>
+              <div className={styles.tableTitle}>来源字段</div>
+              <Table
+                {...props}
+                recordKey="id"
+                records={sourceFields}
+                columns={columns('show')}
+                addons={[
+                  selectable({
+                    value: selectedFields.map(item => item.sourceField.id.toString()),
+                    onChange: (keys, context) => {
+                      const valueMaps = keys.map(key => ({
+                        sourceField: context.selectedRecords.filter(
+                          item => item.id.toString() === key,
+                        )[0],
+                        targetField: targetFields.filter(item => item.id.toString() === key)[0],
+                      }));
+                      setSelectedFields(valueMaps);
+                      onSelect(valueMaps);
+                    },
+                    rowSelectable(rowKey, context) {
+                      if (targetFields[Number(rowKey)]) {
+                        return true;
+                      }
+                      return false;
+                    },
+                  }),
+                ]}
+              />
+            </div>
+            <div>
+              <div style={{ height: '65px' }}></div>
+              <div style={{ height: 'calc(100% - 65px)' }}>
+                {sourceFields.map((item, i) => (
+                  <div
+                    style={{
+                      height: `${100 / sourceFields.length}%`,
+                      lineHeight: '40px',
+                      visibility: selectedFields.map(item => item.sourceField.id).includes(i)
+                        ? 'visible'
+                        : 'hidden',
+                    }}
+                    key={i}
+                  >
+                    <span style={{ fontSize: '20px', color: '#CBCBCB' }}>&#10230;</span>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </Col>
+
+        <Col span={12}>
+          <div className={styles.tableTitle}>
+            <div>写入字段</div>
+            <div className={styles.actions}>
+              {actions.map((action, i) => (
+                <span className={styles.action} key={i}>
+                  {action.icon}
+                  <Button type="link" onClick={action.onClick} disabled={action.disabled}>
+                    {action.text}
+                  </Button>
+                </span>
+              ))}
+            </div>
+          </div>
+          <Table
+            {...props}
+            topTip={status === 'loading' && <StatusTip status="loading" />}
+            recordKey="fieldName"
+            columns={columns('edit')}
+            records={targetFields}
+            addons={[
+              draggable({
+                onDragEnd: (records, context) => {
+                  //change target fields
+                  setTargetFields(records);
+                  //change selected fields
+                  const newSelectedFields = selectedFields.map((selectedField, i) => ({
+                    sourceField: selectedField.sourceField,
+                    targetField: records[selectedField.sourceField?.id],
+                  }));
+                  setSelectedFields(newSelectedFields);
+                  onSelect(newSelectedFields);
+                },
+              }),
+              injectable({
+                row: (props, context) => ({
+                  className: selectedFields.some(
+                    item => item.sourceField?.id === context.recordIndex,
+                  )
+                    ? 'is-selected'
+                    : undefined,
+                }),
+              }),
+            ]}
+          />
+        </Col>
+      </Row>
+      {fieldsParseVisible && (
+        <FieldsParse
+          visible={fieldsParseVisible}
+          onOk={data => {
+            setFieldParseVisible(false);
+            setTargetFields(data);
+          }}
+          onClose={() => setFieldParseVisible(false)}
+        />
+      )}
+    </>
+  );
+};
+
+export default FieldsMap;
--- inlong-dashboard/src/@tencent/pages/ReportDetail/Subscribe/AddSubscribe.tsx
+++ inlong-dashboard/src/@tencent/pages/ReportDetail/Subscribe/AddSubscribe.tsx
@@ -16,134 +16,226 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-import InfoCard from '@/@tencent/components/InfoCard';
-import { Drawer, Button, Icon, Row, Col, Form } from '@tencent/tea-component';
+import ContentRadio from '@/@tencent/components/ContentRadio';
+import FieldsMap from '@/@tencent/components/FieldsMap';
+import { FieldData } from '@/@tencent/components/FieldsMap';
+import { Drawer, Button, Row, Col, Form } from '@tencent/tea-component';
 import { ProForm, ProFormProps } from '@tencent/tea-material-pro-form';
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 
 export interface AddSubscribeDrawerProps {
   visible: boolean;
   onClose: () => void;
 }
 
-const formFields: ProFormProps['fields'] = [
-  {
-    name: 'writeType',
-    type: 'string',
-    title: '写入类型',
-    required: true,
-    component: 'select',
-    appearance: 'button',
-    style: { width: '80%' },
-    defaultValue: 'HIVE',
-    disabled: true,
-    options: [
-      {
-        value: 'HIVE',
-        text: 'HIVE',
-      },
-    ],
-  },
-  {
-    name: 'sperate',
-    type: 'string',
-    title: '分区间隔',
-    required: true,
-    component: 'segment',
-    defaultValue: 'hour',
-    options: [
-      {
-        value: 'hour',
-        text: '小时',
-      },
-      {
-        value: 'day',
-        text: '天',
-      },
-    ],
-    suffix: <span>分区字段：</span>,
-  },
-  {
-    name: 'database',
-    type: 'string',
-    title: '数据库名称',
-    required: true,
-    component: 'select',
-    appearance: 'button',
-    style: { width: '80%' },
-    defaultValue: 'HIVE',
-    options: [
-      {
-        value: 'HIVE',
-        text: 'HIVE',
-      },
-    ],
-    suffix: (
-      <a href="/" target="_blank">
-        申请资源
-      </a>
-    ),
-  },
-  {
-    name: 'tableType',
-    type: 'string',
-    title: '选择表',
-    required: true,
-    component: 'segment',
-    defaultValue: 'auto',
-    options: [
-      {
-        value: 'auto',
-        text: '自动建表',
-      },
-      {
-        value: 'select',
-        text: '选择已有表',
-      },
-    ],
-  },
-  {
-    name: 'table',
-    title: '表名称',
-    type: 'string',
-    required: true,
-    component: 'input',
-    style: { width: '80%' },
-    defaultValue: 'test_table_name',
-    options: [
-      {
-        value: 'test_table_name',
-        text: 'test_table_name',
-      },
-    ],
-    visible: values => values.tableType === 'auto',
-  },
-  {
-    name: 'tableName',
-    title: '表名称',
-    type: 'string',
-    required: true,
-    component: 'select',
-    appearance: 'button',
-    style: { width: '80%' },
-    defaultValue: 'test_table_name',
-    options: [
-      {
-        value: 'test_table_name',
-        text: 'test_table_name',
-      },
-    ],
-    visible: values => values.tableType === 'select',
-  },
-];
-
 const AddSubscribeDrawer = ({ visible, onClose }: AddSubscribeDrawerProps) => {
+  const defaultRecords = [
+    {
+      id: 0,
+      fieldName: '测试longtext：test2352vrgrlmgklrtngktrgnkrtnrtjngrtnnbktrjnvtr',
+      fieldType: 'string',
+      fieldComment: '备注备至备注备至备注备至备注备至备注备至备注备至',
+    },
+    {
+      id: 1,
+      fieldName: 'test1',
+      fieldType: 'string',
+      fieldComment: '备注1',
+    },
+    {
+      id: 2,
+      fieldName: 'test2',
+      fieldType: 'string',
+      fieldComment: '备注1',
+    },
+    {
+      id: 3,
+      fieldName: 'test3',
+      fieldType: 'string',
+      fieldComment: '备注1',
+    },
+    {
+      id: 4,
+      fieldName: 'test4',
+      fieldType: 'string',
+      fieldComment: '备注1',
+    },
+  ];
+  const [targetFields, setTargetFields] = useState<FieldData>(defaultRecords);
+  const [selectedFields, setSelectedFields] = useState<any>([]);
+  const [isFieldsMapVisible, setIsFieldsMapVisible] = useState<boolean>(false);
   const formRef = useRef<any>();
+  const autoCreateTableRef = useRef<any>();
+  const selectTableRef = useRef<any>();
+
+  const formFields: ProFormProps['fields'] = [
+    {
+      name: 'writeType',
+      type: 'string',
+      title: '写入类型',
+      required: true,
+      component: 'select',
+      appearance: 'button',
+      style: { width: '80%' },
+      defaultValue: 'HIVE',
+      disabled: true,
+      options: [
+        {
+          value: 'HIVE',
+          text: 'HIVE',
+        },
+      ],
+    },
+    {
+      name: 'sperate',
+      type: 'string',
+      title: '分区间隔',
+      required: true,
+      component: 'radio',
+      defaultValue: 'hour',
+      options: [
+        {
+          name: 'hour',
+          title: '小时',
+        },
+        {
+          name: 'day',
+          title: '天',
+        },
+      ],
+      reaction: (fields, values) => {
+        fields.setComponentProps({
+          suffix: (
+            <span style={{ color: '#888', display: 'inline-block', marginLeft: '10px' }}>
+              分区字段：{values.sperate}
+            </span>
+          ),
+        });
+      },
+    },
+    {
+      name: 'database',
+      type: 'string',
+      title: '数据库名称',
+      required: true,
+      component: 'select',
+      appearance: 'button',
+      style: { width: '80%' },
+      defaultValue: 'HIVE',
+      options: [
+        {
+          value: 'HIVE',
+          text: 'HIVE',
+        },
+      ],
+      suffix: (
+        <a href="/" target="_blank">
+          申请资源
+        </a>
+      ),
+    },
+    {
+      name: 'tableType',
+      type: 'string',
+      title: '数据来源',
+      required: true,
+      component: ContentRadio,
+      reaction: (field, values) => {
+        field.setComponentProps({
+          style: { width: '80%' },
+          radios: [
+            {
+              value: 'auto',
+              text: '自动建表',
+              operations: (
+                <ProForm
+                  style={{ padding: '0', marginBottom: '-10px' }}
+                  fields={[
+                    {
+                      name: 'tableName',
+                      type: 'string',
+                      title: '表名称',
+                      required: true,
+                      defaultValue: 'test_table_name',
+                    },
+                  ]}
+                  onRef={form => (autoCreateTableRef.current = form)}
+                  submitter={false}
+                />
+              ),
+            },
+            {
+              value: 'select',
+              text: '选择已有表',
+              operations: (
+                <ProForm
+                  style={{ padding: '0', marginBottom: '-10px' }}
+                  fields={[
+                    {
+                      name: 'tableName',
+                      type: 'string',
+                      title: 'test',
+                      component: 'select',
+                      required: true,
+                      appearance: 'button',
+                      size: 'full',
+                      options: [
+                        {
+                          value: 'test_table_name',
+                          text: 'test_table_name',
+                        },
+                      ],
+                      defaultValue: 'test_table_name',
+                    },
+                  ]}
+                  onRef={form => (selectTableRef.current = form)}
+                  submitter={false}
+                />
+              ),
+            },
+          ],
+        });
+      },
+    },
+  ];
+
+  const onSelect = data => {
+    setSelectedFields(data);
+  };
+
+  const getTargetFields = async (table: string) => {
+    //根据选择的table获取字段 todo
+    const res = await new Promise(resolve => {
+      setTimeout(() => {
+        resolve([
+          {
+            id: 0,
+            fieldName: 'test',
+            fieldType: 'string',
+            fieldComment: '备注',
+          },
+        ]);
+      }, 1000);
+    });
+    return res;
+  };
+
+  //handle add subscrible
   const handleOk = () => {
     formRef.current.submit(values => {
       console.log(values);
     });
+    autoCreateTableRef.current &&
+      autoCreateTableRef.current.submit(values => {
+        console.log(values);
+      });
+    selectTableRef.current &&
+      selectTableRef.current.submit(values => {
+        console.log(values);
+      });
+    console.log(selectedFields);
   };
+
   return (
     <Drawer
       style={{ width: '100%', zIndex: 1000 }}
@@ -166,13 +258,14 @@ const AddSubscribeDrawer = ({ visible, onClose }: AddSubscribeDrawerProps) => {
       }
       onClose={onClose}
     >
-      <Row style={{ padding: '0 8%' }}>
+      <Row style={{ padding: '0 8%' }} gap={20}>
         <Col span={24}>
           <p style={{ fontSize: '16px', fontWeight: 'bold' }}>配置数据源信息</p>
         </Col>
-        <Col span={11}>
-          <InfoCard title="数据来源" icon={<Icon type="table"></Icon>} style={{ height: '300px' }}>
+        <Col span={12}>
+          <div style={{ height: '300px', border: '1px solid #E2E2E2', padding: '20px 20px' }}>
             <Form>
+              <Form.Title>数据来源</Form.Title>
               <Form.Item label="接入方式">
                 <Form.Text>SDK</Form.Text>
               </Form.Item>
@@ -192,22 +285,43 @@ const AddSubscribeDrawer = ({ visible, onClose }: AddSubscribeDrawerProps) => {
                 <Form.Text>分号</Form.Text>
               </Form.Item>
             </Form>
-          </InfoCard>
+          </div>
         </Col>
-        <Col span={2}></Col>
-        <Col span={11}>
-          <InfoCard title="写入信息" icon={<Icon type="table"></Icon>} style={{ height: '300px' }}>
+        <Col span={12}>
+          <div style={{ height: '300px', border: '1px solid #E2E2E2', padding: '20px 20px' }}>
+            <Form.Title>写入信息</Form.Title>
             <ProForm
               fields={formFields}
               submitter={false}
               onRef={form => (formRef.current = form)}
+              onFormValuesChange={form => {
+                //listen form values change
+                const tableType = form.getValuesIn('tableType');
+                if (tableType === 'auto' || tableType === 'select') {
+                  setIsFieldsMapVisible(true);
+                }
+              }}
             />
-          </InfoCard>
+          </div>
         </Col>
-        <Col span={24}>
-          <p style={{ fontSize: '16px', fontWeight: 'bold' }}>字段映射</p>
-          <p>选择需要订阅的来源字段，并对齐写入字段，注意：请确定字段为一一映射关系。</p>
-        </Col>
+        {isFieldsMapVisible === true && (
+          <>
+            <Col span={24}>
+              <p style={{ fontSize: '16px', fontWeight: 'bold' }}>字段映射</p>
+              <p>选择需要订阅的来源字段，并对齐写入字段，注意：请确定字段为一一映射关系。</p>
+            </Col>
+            <Col span={24}>
+              <FieldsMap
+                compact={true}
+                bordered={true}
+                sourceFields={defaultRecords}
+                targetFields={targetFields}
+                onSelect={onSelect}
+                getTargetFields={getTargetFields}
+              />
+            </Col>
+          </>
+        )}
       </Row>
     </Drawer>
   );
